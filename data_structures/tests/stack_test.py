daa7ab9821e12300fcaca88c9f2b96cd13f0f77a
YuriBittencourt <yuri.bittencourt@acad.pucrs.br>
2018-10-21 21:35:17 -0300

    Stack Test avançando

--- data_structures/tests/stack_test.py
+++ data_structures/tests/stack_test.py
@@ -1,7 +1,40 @@
 from data_structures.stack import stack
 import copy
-my_stack = stack()
-my_stack2 = stack()
+from random import random
+
+if __name__ == "__main__":
+    print("-" * 50, "\nRunning the stack constructor with size greather than 0 test case")
+    try:
+        my_stack = stack()
+        assert type(my_stack) == stack
+        size = int(random()*10+1)
+        my_stack2 = stack(size)
+        print("Passed!")
+    except:
+        print("Failed!")
+    print("-" * 50, "\nRunning the less than 1 size stack test case")
+    try:
+        stack(int(random()*-10))
+        print("Failed!")
+    except:
+        print("Passed!")
+    print("-" * 50, "\nRunning the get_max_capacity method test case")
+    try:
+        assert my_stack.get_max_capacity()==None
+        assert my_stack2.get_max_capacity()==size
+        print("Passed!")
+    except:
+        print("Failed!")
+    print("-" * 50, "\nRunning the is_empty method test case")
+    try:
+        assert my_stack.is_empty()
+        my_stack.push("SOMETHING")
+        assert not my_stack.is_empty()
+        print("Passed!")
+    except:
+        print("Failed!")
+    print("-" * 50)
+
 print(my_stack==my_stack2)
 my_stack.push("world")
 print(my_stack==my_stack2)
