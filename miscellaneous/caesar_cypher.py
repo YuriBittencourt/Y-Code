3289329341c9cf683b9dcc3a7bffbd3563d53bd9
YuriBittencourt <yuri.bittencourt@acad.pucrs.br>
2018-12-03 09:18:04 -0200

    caesar_cypher modificado pra classe
    Signed-off-by: YuriBittencourt <yuri.bittencourt@totvs.com.br>

--- miscellaneous/caesar_cypher.py
+++ miscellaneous/caesar_cypher.py
@@ -1,27 +1,28 @@
-__alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-__chr_num_dict = dict(zip(__alphabet, range(0, len(__alphabet))))
-__num_chr_dict = dict(zip(range(0, len(__alphabet)), __alphabet))
+class caesar_cypher:
 
+    def __init__(self):
+        self.__alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.__chr_num_dict = dict(zip(self.__alphabet, range(0, len(self.__alphabet))))
+        self.__num_chr_dict = dict(zip(range(0, len(self.__alphabet)), self.__alphabet))
 
-def encrypt(sentence, offset=3):
-    sentence = sentence.upper()
-    new_str = ''
-    for char in sentence:
-        if char in __chr_num_dict:
-            index = (__chr_num_dict[char.upper()] + offset) % len(__alphabet)
-            new_str += __num_chr_dict[index]
-        else:
-            new_str += char
-    return new_str
+    def encrypt(self, sentence, offset=3):
+        sentence = sentence.upper()
+        new_str = ''
+        for char in sentence:
+            if char in self.__chr_num_dict:
+                index = (self.__chr_num_dict[char.upper()] + offset) % len(self.__alphabet)
+                new_str += self.__num_chr_dict[index]
+            else:
+                new_str += char
+        return new_str
 
-
-def decrypt(sentence, offset=3):
-    sentence = sentence.upper()
-    new_str = ''
-    for char in sentence:
-        if char in __chr_num_dict:
-            index = (__chr_num_dict[char.upper()] - offset) % len(__alphabet)
-            new_str += __num_chr_dict[index]
-        else:
-            new_str += char
-    return new_str
+    def decrypt(self, sentence, offset=3):
+        sentence = sentence.upper()
+        new_str = ''
+        for char in sentence:
+            if char in self.__chr_num_dict:
+                index = (self.__chr_num_dict[char.upper()] - offset) % len(self.__alphabet)
+                new_str += self.__num_chr_dict[index]
+            else:
+                new_str += char
+        return new_str
